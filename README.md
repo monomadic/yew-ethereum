d7d1ec0f91afc9c39f56c5e97d5e9a866ff5a0dd
monomadic <monomadic@monomadic>
2022-05-30 23:31:55 +0700

    nix support

--- README.md
+++ README.md
@@ -6,21 +6,34 @@ such as metamask.
 
 ## Installation
 
-First, install cargo dependencies (if you don't have them).
+### Cargo
 
 ```bash
+rustup default nightly
+
+# First, install cargo dependencies (if you don't have them).
 cargo install trunk
 cargo install cargo-generate
-```
 
-Then generate a new project with this template.
-
-```bash
+# Then generate a new project with this template.
 cargo generate monomadic/yew-ethereum-template
-```
 
-Then start the trunk server.
-
-```bash
+# Then start the trunk server.
 trunk serve
 ```
+
+### Nix
+
+```bash
+nix develop
+trunk serve
+```
+
+## Recreating This Template
+
+If you don't want to use cargo generate, here are the basic steps to reproduce this
+project.
+
+## Resources
+- https://yew.rs/
+- https://trunkrs.dev/
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use wasm_bindgen_futures::spawn_local;
+use web3::{
+    futures::StreamExt,
+    transports::eip_1193::{Eip1193, Provider},
+};
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+
+    // connect wallet thread
+    spawn_local(async {
+        let provider = Provider::default().unwrap().unwrap();
+        connect_wallet(provider).await.unwrap();
+    });
+
+    // on accounts changed
+    spawn_local(async {
+        let provider = Provider::default().unwrap().unwrap();
+        let transport = Eip1193::new(provider);
+        let mut stream = transport.accounts_changed_stream();
+
+        while let Some(accounts) = stream.next().await {
+            log::info!("accounts changed {:?}", &accounts);
+        }
+    });
+
+    // on chain changed
+    spawn_local(async {
+        let provider = Provider::default().unwrap().unwrap();
+        let transport = Eip1193::new(provider);
+        let mut stream = transport.chain_changed_stream();
+
+        while let Some(chainid) = stream.next().await {
+            log::info!("chain changed {:?}", &chainid);
+        }
+    });
+}
+
+async fn connect_wallet(provider: Provider) -> web3::Result<()> {
+    let transport = Eip1193::new(provider);
+    let web3 = web3::Web3::new(transport);
+    let accounts = web3.eth().request_accounts().await.unwrap();
+
+    for account in accounts {
+        let balance = web3.eth().balance(account, None).await.unwrap();
+        log::info!("Balance of {:?}: {}", account, balance);
+    }
+
+    Ok(())
+}
